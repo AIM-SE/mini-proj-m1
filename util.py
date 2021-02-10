ed72f3c9b06f3e9c2e5cb3d324b641a8505b53e2
hirussellkim <hi.alex.kim3@gmail.com>
2021-02-10 11:44:17 +0800

    Update util.py
    update the model path

--- util.py
+++ util.py
@@ -29,13 +29,13 @@ def set_env(root_path='.', kind='ml'):
     if not 'SM_CHANNEL_TRAIN' in os.environ :
         os.environ['SM_CHANNEL_TRAIN'] = '%s/data-%s/' % (root_path, kind)
     if not 'SM_MODEL_DIR' in os.environ:
-        os.environ['SM_MODEL_DIR'] = '%s/output/' % root_path
+        os.environ['SM_MODEL_DIR'] = '%s/model/' % root_path
 
     # for inference
     if not 'SM_CHANNEL_EVAL' in os.environ :
         os.environ['SM_CHANNEL_EVAL'] = '%s/data-%s/' % (root_path, kind)
     if not 'SM_CHANNEL_MODEL' in os.environ :
-        os.environ['SM_CHANNEL_MODEL'] = '%s/output/' % root_path
+        os.environ['SM_CHANNEL_MODEL'] = '%s/model/' % root_path
     if not 'SM_OUTPUT_DATA_DIR' in os.environ :
         os.environ['SM_OUTPUT_DATA_DIR'] = '%s/output/' % root_path
 
